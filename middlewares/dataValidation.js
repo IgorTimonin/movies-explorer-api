efc50c33a24b85936b13bdf1ed9bfcf3a06ac2f8
IgorTimonin <mut4nt@yandex.ru>
2022-09-09 01:36:04 +0300

    name required validation fix

--- middlewares/dataValidation.js
+++ middlewares/dataValidation.js
@@ -12,7 +12,7 @@ module.exports.createUserValidator = {
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
   }),
 };
 
