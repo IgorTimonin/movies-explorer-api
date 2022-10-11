e47014e66506d05cb37cad67d2ac828b8bb1c730
Igor Timonin <mut4nt@yandex.ru>
2022-10-11 12:22:25 +0300

    change option of limiter

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -2,7 +2,7 @@ const limiter = require('express-rate-limit');
 
 module.exports.rateLimiter = limiter({
   windowMs: 24 * 60 * 60 * 1000, // 24 часа
-  max: 100,
+  max: 1000,
   message: 'Превышено кол-во запросов за 24 часа с одного ip',
   standardHeaders: true,
   legacyHeaders: false,
